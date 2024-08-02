ad13ef7ed566f9ebcce3a50c19ebed48f47b8176
rewqla <yurii.mosiichuk@renome.ua>
2024-08-02 09:58:47 +0300

    Added delete request

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ import { check } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 20 }, // ramp up to 20 users
+    { duration: '10s', target: 20 }, // ramp up to 20 users
     { duration: '20s', target: 20 },  // stay at 20 users for 20 s
     { duration: '15s', target: 0 },  // ramp down to 0 users
   ],
 };
-
+//delete 
+//put
+//refactor into a modules
+//for 
+//some request in one time (using functions)
+//ready data 
+//random data
+//random data from ready data
+//grpc
 export default function () {
-    const url='https://dummyjson.com/auth/login';
+    const randomId = Math.floor(Math.random() * 195);
+    const url=`https://dummyjson.com/products/${randomId}`;
 
-    const payload = JSON.stringify({
-        username: 'emilys',
-        password: 'emilyspass',
-    });
+    const res = http.del(url);
     
-    const headers = {
-        'Content-Type': 'application/json',
-    };
-
-    const res = http.post(url, payload, { headers: headers });
+    console.log(`Deleting product with ID: ${randomId}`);
 
     check(res, {
       'Status is 200': (response) => response.status === 200,
+      'Is deleted': (response) => JSON.parse(response.body).isDeleted === true
     }); 
 
     sleep(1);
